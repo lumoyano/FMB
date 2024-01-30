4190795a9757d4eb57d8c0f41db7b394b73e9ef1
lumoyano <121978205+lumoyano@users.noreply.github.com>
2024-01-29 19:26:48 -0700

    setup for javafx GUI

--- src/main/java/fmb/DataEntry.java
+++ src/main/java/fmb/DataEntry.java
@@ -1,7 +1,15 @@
 package fmb;
 
-public class DataEntry {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class DataEntry extends Application {
     public static void main(String[] args) {
-        
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.show();
     }
 }
