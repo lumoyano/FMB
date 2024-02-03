5cdef223ff889662f75aa258058542e732d39e71
lumoyano <121978205+lumoyano@users.noreply.github.com>
2024-02-03 08:26:57 -0700

    Scraper created

--- src/main/fmb/DataEntry.java
+++ src/main/fmb/DataEntry.java
@@ -4,8 +4,12 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import main.fmb.tools.UI;
 
+import main.fmb.tools.ScraperTool;
+
+import java.io.IOException;
+
 public class DataEntry extends Application {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         launch(args);
     }
 
--- src/main/fmb/tools/ScraperTool.java
+++ src/main/fmb/tools/ScraperTool.java
@@ -1,4 +1,30 @@
 package main.fmb.tools;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+
 public class ScraperTool {
+
+    public static void main(String[] args) throws IOException {
+        String test = getSoup("https://incidecoder.com/products/mac-lipglass");
+    }
+
+    public static String getSoup(String link) throws IOException {
+        Document doc = Jsoup.connect(link).get();
+        Element container = doc.getElementById("showmore-section-ingredlist-short");
+        Elements hrefs = container.getElementsByClass("ingred-link");
+        ArrayList<String> listItems = new ArrayList<>();
+        for (Element e :
+                hrefs) {
+            listItems.add(e.text());
+        }
+        System.out.println(listItems);
+        return listItems.toString();
+    }
 }
