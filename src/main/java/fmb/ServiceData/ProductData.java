a3ab133157c1856d883c8eaccbf56a0bad46558a
lumoyano <121978205+lumoyano@users.noreply.github.com>
2024-02-05 22:18:30 -0700

    query db for everything
    have to come back and finish storing into instance

--- src/main/java/fmb/ServiceData/ProductData.java
+++ src/main/java/fmb/ServiceData/ProductData.java
@@ -1,7 +1,10 @@
 package fmb.ServiceData;
 
 import fmb.model.Product;
+import fmb.tools.DBConfig;
 
+import javax.swing.plaf.nimbus.State;
+import java.sql.*;
 import java.util.ArrayList;
 
 public class ProductData {
@@ -26,6 +29,26 @@ public class ProductData {
     }
 
     private void loadDataFromDB() {
+            String url = DBConfig.getDatabaseUrl();
+            String username = DBConfig.getDBUsername();
+            String password = DBConfig.getDBPassword();
 
+            try {
+                Connection db = DriverManager.getConnection(url, username, password);
+                Statement st = db.createStatement();
+                ResultSet resultSet = st.executeQuery("SELECT * FROM PRODUCT");
+                while (resultSet.next()) {
+                    /*
+                    column_name	data_type
+                    productid	integer
+                    productbrand	integer
+                    productname	character varying
+                    ingredients	text
+                    producttype	character varying
+                    */
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
     }
 }
