1a34b89690c7a6fb321933f02227636f7ab54b8e
lumoyano <121978205+lumoyano@users.noreply.github.com>
2024-02-10 23:17:25 -0700

    Refresh function implemented

--- src/main/java/fmb/controller/ControllerMain.java
+++ src/main/java/fmb/controller/ControllerMain.java
@@ -1,4 +1,21 @@
 package fmb.controller;
 
+import fmb.serviceData.ProductData;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+
 public class ControllerMain {
+    @FXML
+    private void refreshData(){
+        ProductData.getInstance().refreshData();
+        showAlert("Data Refreshed", "Data has been refreshed successfully!");
+    }
+
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 }
