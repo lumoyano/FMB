64e1925152de7763eedb9c6f3496416a7946816c
lumoyano <121978205+lumoyano@users.noreply.github.com>
2024-02-10 23:49:18 -0700

    first try at populating table

--- src/main/java/fmb/controller/ControllerMain.java
+++ src/main/java/fmb/controller/ControllerMain.java
@@ -1,10 +1,49 @@
 package fmb.controller;
 
+import fmb.model.Product;
 import fmb.serviceData.ProductData;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class ControllerMain {
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+public class ControllerMain implements Initializable {
+    @FXML
+    private TableView<Product> tableView;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        TableColumn<Product, Integer> idColumn = new TableColumn<>("ID");
+        idColumn.setCellValueFactory(new PropertyValueFactory<>("productID"));
+
+        TableColumn<Product, String> nameColumn = new TableColumn<>("Name");
+        nameColumn.setCellValueFactory(new PropertyValueFactory<>("productName"));
+
+        TableColumn<Product, String> brandColumn = new TableColumn<>("Brand");
+        brandColumn.setCellValueFactory(new PropertyValueFactory<>("productBrand"));
+
+        TableColumn<Product, String> typeColumn = new TableColumn<>("Type");
+        brandColumn.setCellValueFactory(new PropertyValueFactory<>("productType"));
+
+        tableView.getColumns().addAll(idColumn, nameColumn, brandColumn, typeColumn);
+
+        // Populate TableView with data
+        ArrayList<Product> instance = ProductData.getInstance().getCurrentList();
+        ObservableList<Product> products = FXCollections.observableArrayList(
+                instance
+        );
+        tableView.setItems(products);
+    }
+
+
     @FXML
     private void refreshData(){
         ProductData.getInstance().refreshData();
@@ -18,4 +57,5 @@ public class ControllerMain {
         alert.setContentText(message);
         alert.showAndWait();
     }
+
 }
