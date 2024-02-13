cebf7f658281adbf91711f6ca74365ccb31cf24c
lumoyano <121978205+lumoyano@users.noreply.github.com>
2024-02-12 20:16:48 -0700

    Update ProductData.java
    globalized params for connection.
    Created CRUD methods. More work has to be done before they're functional.
    Try with Resource encapsulations

--- src/main/java/fmb/serviceData/ProductData.java
+++ src/main/java/fmb/serviceData/ProductData.java
@@ -3,17 +3,24 @@ package fmb.serviceData;
 import fmb.model.Product;
 import fmb.tools.DBConfig;
 
+import java.net.URL;
 import java.sql.*;
 import java.util.ArrayList;
 
 public class ProductData {
     private static ArrayList<Product> currentList = new ArrayList<>();
 
+    /*
+    Full link has a format jdbc:postgresql://server:port5432/defaultdatabase?userparam&passwdparam
+    ElephantSQL provides a link that is both incorrect and misspelled, Use this as a rule
+     */
+    private final String URL = DBConfig.getDatabaseUrl(); //jdbc:postgresql://server:port5432/defaultdatabase
+    private final String USERNAME = DBConfig.getDBUsername();
+    private final String PASSWORD = DBConfig.getDBPassword();
+
     private static ProductData instance = new ProductData();
 
-    private ProductData() {
-
-    }
+    private ProductData() {}
 
     public static ProductData getInstance() {
         return instance;
@@ -28,13 +35,7 @@ public class ProductData {
     }
 
     private static void loadDataFromDB() {
-        /*
-        Full link has a format jdbc:postgresql://server:port5432/defaultdatabase?userparam&passwdparam
-        ElephantSQL provides a link that is both incorrect and misspelled, Use this as a rule
-         */
-        String url = DBConfig.getDatabaseUrl(); //jdbc:postgresql://server:port5432/defaultdatabase
-        String username = DBConfig.getDBUsername();
-        String password = DBConfig.getDBPassword();
+
 
         try {
             Class.forName("org.postgresql.Driver");
@@ -42,9 +43,9 @@ public class ProductData {
             System.out.println(e.getMessage());
         }
 
-        try {
-            Connection db = DriverManager.getConnection(url, username, password);
-            Statement st = db.createStatement();
+        try (Connection db = DriverManager.getConnection(getInstance().URL, getInstance().USERNAME, getInstance().PASSWORD);
+             Statement st = db.createStatement()) {
+
             ResultSet resultSet = st.executeQuery("SELECT * FROM PRODUCT");
             currentList.clear();
             while (resultSet.next()) {
@@ -70,11 +71,47 @@ public class ProductData {
                 Product currentRow = new Product(productID, productBrand, productName, ingredients, productType);
                 currentList.add(currentRow);
             }
-            st.close();
-            db.close();
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
+    public void addProduct(Product product) { //Unfinished method
+        try (Connection db = DriverManager
+                .getConnection(getInstance().URL, getInstance().USERNAME, getInstance().PASSWORD);
+             PreparedStatement statement = db.prepareStatement("INSERT INTO products (product_name) VALUES (?)")) {
+
+            statement.setString(1, product.getProductName());
+            // Set other parameters...
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateProduct(Product product) { //Unfinished method
+        try (Connection db = DriverManager
+                .getConnection(getInstance().URL, getInstance().USERNAME, getInstance().PASSWORD);
+             PreparedStatement statement = db.prepareStatement("UPDATE products SET product_name = ? WHERE product_id = ?")) {
+
+            statement.setString(1, product.getProductName());
+            // Set other parameters...
+            statement.setInt(2, product.getProductID());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteProduct(int productId) {
+        try (Connection db = DriverManager
+                    .getConnection(getInstance().URL, getInstance().USERNAME, getInstance().PASSWORD);
+            PreparedStatement statement = db.prepareStatement("DELETE FROM products WHERE product_id = ?")) {
+
+            statement.setInt(1, productId);
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
