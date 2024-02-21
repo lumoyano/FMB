3b0d8af48c3f38403824e2c593839207e5e12b04
lumoyano <121978205+lumoyano@users.noreply.github.com>
2024-02-20 21:06:42 -0700

    setConnection popup method

--- src/main/java/fmb/controller/ControllerMain.java
+++ src/main/java/fmb/controller/ControllerMain.java
@@ -5,13 +5,20 @@ import fmb.serviceData.ProductData;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
@@ -68,6 +75,7 @@ public class ControllerMain implements Initializable {
         ProductData.getInstance().deleteProduct(1);
     }
 
+    @FXML
     private void showAlert(String title, String message) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
         alert.setTitle(title);
@@ -75,6 +83,24 @@ public class ControllerMain implements Initializable {
         alert.setContentText(message);
         alert.showAndWait();
     }
-    
 
+    @FXML
+    private void setConnection() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../../resources/view/ConnectionPopUp.fxml"));
+            Parent root = loader.load();
+            ConnectionPopUpController controller = loader.getController();
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @FXML
+    public void save (ActionEvent event) {
+        showAlert("Im a message,", "ya dummy");
+    }
 }
