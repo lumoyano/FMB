0f74e35a781c89797444f167a39100095cce5cec
lumoyano <121978205+lumoyano@users.noreply.github.com>
2024-02-21 20:40:20 -0700

    set and clear properties

--- src/main/java/fmb/tools/DBConfig.java
+++ src/main/java/fmb/tools/DBConfig.java
@@ -1,7 +1,9 @@
 package fmb.tools;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Properties;
 
 public class DBConfig {
@@ -31,4 +33,36 @@ public class DBConfig {
     public static String getDBPassword() {
         return properties.getProperty("DB_PASSWORD");
     }
+
+    public static void setProperties(String url, String username, String password) {
+        properties.setProperty("DB_URL", url);
+        properties.setProperty("DB_USERNAME", username);
+        properties.setProperty("DB_PASSWORD", password);
+
+        try {
+            // Write properties to the properties file
+            try (OutputStream output = new FileOutputStream(CONFIG_FILE_PATH)) {
+                properties.store(output, "Database Properties");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void clearProperties() {
+        properties.remove("DB_URL");
+        properties.remove("DB_USERNAME");
+        properties.remove("DB_PASSWORD");
+
+        try {
+            // Write properties to the properties file
+            try (OutputStream output = new FileOutputStream(CONFIG_FILE_PATH)) {
+                properties.store(output, "Database Properties");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            // Handle the exception (e.g., show an error message)
+        }
+    }
+
 }
