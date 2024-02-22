dda7e467e1944f937c2cb4cdf8395b9924d558c3
lumoyano <121978205+lumoyano@users.noreply.github.com>
2024-02-21 20:39:52 -0700

    set input to properties file

--- src/main/java/fmb/controller/ConnectionPopUpController.java
+++ src/main/java/fmb/controller/ConnectionPopUpController.java
@@ -1,14 +1,44 @@
 package fmb.controller;
 
+import fmb.tools.DBConfig;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.awt.event.ActionEvent;
 
 public class ConnectionPopUpController {
     @FXML
-    public void save () {
+    private Stage stage;
 
+    @FXML
+    private TextField urlField;
+    @FXML
+    private TextField usernameField;
+    @FXML
+    private TextField passwdField;
+
+
+    @FXML
+    public void save () {
+        try {
+            DBConfig.setProperties(
+                    urlField.getText(),
+                    usernameField.getText(),
+                    passwdField.getText()
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //notify success and close
+        showAlertAndClose("Connection", "Information saved. Please refresh data before modifying the tables.");
+    }
+
+    @FXML
+    public void clearConnection() {
+        
     }
 
     @FXML
@@ -17,6 +47,16 @@ public class ConnectionPopUpController {
         alert.setTitle(title);
         alert.setHeaderText(null);
         alert.setContentText(message);
+
+        alert.setOnCloseRequest(dialogEvent -> {
+            alert.close();
+            stage.close();
+        });
+
         alert.showAndWait();
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
     }
 }
