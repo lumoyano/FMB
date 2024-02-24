96a666760547259a2f08fc98e69e1c4a05f9c2b7
lumoyano <121978205+lumoyano@users.noreply.github.com>
2024-02-23 20:54:55 -0700

    Error handling

--- src/main/java/fmb/controller/NewRowController.java
+++ src/main/java/fmb/controller/NewRowController.java
@@ -2,9 +2,11 @@ package fmb.controller;
 
 import fmb.model.Product;
 import fmb.serviceData.ProductData;
+import fmb.tools.ErrorTool;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -24,14 +26,42 @@ public class NewRowController {
     private TextArea ingredientsArea;
 
     @FXML
+    private Stage stage;
+
+    @FXML
     public void saveChanges() {
         String[] individualIngs = ingredientsArea.getText().split("\\s*,\\s*");
         ArrayList<String> ingredients = new ArrayList<>(Arrays.asList(individualIngs));
+
+        //guard clauses for no ID and for empty fields
+        if (ProductData.getInstance().getNextID() == -1) {
+            ErrorTool.showAlertAndClose("Error", "There was an error creating your entry: Next available ID returned -1", stage);
+            return;
+        }
+        if (productBrandField.getText().isEmpty() ||
+                productNameField.getText().isEmpty() ||
+                productTypeField.getText().isEmpty() ||
+                ingredientsArea.getText().isEmpty()){
+            ErrorTool.showAlert("Error", "There was an error creating your entry. Please make sure all fields are filled.");
+            return;
+        }
         Product newRow = new Product(ProductData.getInstance().getNextID(),
                 productBrandField.getText(),
                 productNameField.getText(),
                 ingredients,
                 productTypeField.getText());
-        ProductData.getInstance().addProduct(newRow);
+        try{
+            ProductData.getInstance().addProduct(newRow);
+            ErrorTool.showAlertAndClose("New Row added",
+                    "Succesfully added row. Please refresh to see changes", stage);
+        } catch (Exception e) {
+            ErrorTool.showAlert("Error", "There was an error creating your entry");
+        }
+
+    }
+
+    @FXML
+    public void setStage(Stage s) {
+        this.stage = s;
     }
 }
