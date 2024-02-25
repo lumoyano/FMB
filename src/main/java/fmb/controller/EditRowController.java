3cace5492f5bd0074471e0bf312abba09771d4cf
lumoyano <121978205+lumoyano@users.noreply.github.com>
2024-02-25 00:18:30 -0700

    Update EditRowController.java

--- src/main/java/fmb/controller/EditRowController.java
+++ src/main/java/fmb/controller/EditRowController.java
@@ -2,16 +2,25 @@ package fmb.controller;
 
 import fmb.model.Product;
 import fmb.serviceData.ProductData;
+import fmb.serviceData.ScraperData;
 import fmb.tools.ErrorTool;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.ResourceBundle;
 
-public class EditRowController {
+public class EditRowController implements Initializable {
 
     @FXML
     private TextField productNameField;
@@ -28,40 +37,93 @@ public class EditRowController {
     @FXML
     private Stage stage;
 
+    private Product product;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        product = ControllerMain.getCurrentRow();
+        populateFields();
+
+    }
+
     @FXML
     public void saveChanges() {
         String[] individualIngs = ingredientsArea.getText().split("\\s*,\\s*");
         ArrayList<String> ingredients = new ArrayList<>(Arrays.asList(individualIngs));
 
-        //guard clauses for no ID and for empty fields
-        if (ProductData.getInstance().getNextID() == -1) {
-            ErrorTool.showAlertAndClose("Error", "There was an error creating your entry: Next available ID returned -1", stage);
-            return;
-        }
         if (productBrandField.getText().isEmpty() ||
                 productNameField.getText().isEmpty() ||
                 productTypeField.getText().isEmpty() ||
                 ingredientsArea.getText().isEmpty()){
-            ErrorTool.showAlert("Error", "There was an error creating your entry. Please make sure all fields are filled.");
+            ErrorTool.showAlert("Error", "There was an error changing your entry. Please make sure all fields are filled.");
             return;
         }
-        Product newRow = new Product(ProductData.getInstance().getNextID(),
+        Product newRow = new Product(ControllerMain.getCurrentRow().getProductID(),
                 productBrandField.getText(),
                 productNameField.getText(),
-                ingredients,
-                productTypeField.getText());
+                productTypeField.getText(),
+                ingredients);
         try{
-            ProductData.getInstance().addProduct(newRow);
-            ErrorTool.showAlertAndClose("New Row added",
-                    "Succesfully added row. Please refresh to see changes", stage);
+            ProductData.getInstance().updateProduct(newRow);
+            ErrorTool.showAlertAndClose("Edit Row",
+                    "Changes to row were made. Please refresh to see changes", stage);
         } catch (Exception e) {
-            ErrorTool.showAlert("Error", "There was an error creating your entry");
+            ErrorTool.showAlert("Error", "There was an error changing your entry.");
         }
 
+    }
+
+    public void populateFields() {
+        if (product == null) return;
+        // Populate text fields with product data
+        productNameField.setText(product.getProductName());
+        productBrandField.setText(product.getProductBrand());
+        productTypeField.setText(product.getProductType());
+
+        // Populate text area with ingredients from link if present, and by product default
+        StringBuilder ingredientsText = new StringBuilder();
+        if (ScraperData.getInstance().getScrapedIngredients().isEmpty()){
+            for (String ingredient :
+                    product.getIngredients()) {
+                ingredientsText.append(ingredient).append(",\n"); // Append each ingredient with a newline
+            }
+        } else {
+            for (String ingredient :
+                    ScraperData.getInstance().getScrapedIngredients()) {
+                ingredientsText.append(ingredient).append(",\n");
+            }
+            ScraperData.getInstance().clearScrapedIngredients();
+        }
+        ingredientsArea.setText(ingredientsText.toString());
     }
 
     @FXML
     public void setStage(Stage s) {
         this.stage = s;
+    }
+
+    @FXML
+    public void openScrapeView() {
+        try {
+            //load popup view
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("../../resources/view/LinkPopUp.fxml"));
+            Parent root = loader.load();
+            //set respective controller
+            LinkPopUpController controller = loader.getController();
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setTitle("Connection setup");
+            stage.setScene(new Scene(root));
+            //Setup to close popup by using stage.close within alert. See ShowAlertAndClose
+            controller.setStage(stage);
+            stage.showAndWait();
+            populateFields();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
     }
 }
